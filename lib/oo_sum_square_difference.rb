275b791dbab9ca50241c1b91fc2a706f96101a89
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-30 09:35:27 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize (num)
+    @array = Array(1..num)
+  end
+
+  def square_of_sum
+    @array.sum * @array.sum
+  end
+
+  def sum_of_squares
+    squares = @array.map { |num| num * num }
+    sum_of_squares = squares.sum
+  end
+
+  def difference
+    self.square_of_sum - self.sum_of_squares
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,11 @@
 # Implement your procedural solution here!
+def sum_square_difference(num) 
+  array = Array(1..num)
+
+  square_of_sum = array.sum * array.sum
+
+  squares = array.map {|num| num * num }
+  sum_of_squares = squares.sum
+
+  difference = square_of_sum - sum_of_squares
+end
